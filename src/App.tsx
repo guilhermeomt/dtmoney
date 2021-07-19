21dbcd4525d07f208a215e58463e4711692c44fa
Guilherme <guilhermet213@gmail.com>
2021-07-19 20:03:07 -0300

    feat: implement header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Header } from "./components/Header";
+
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
+    <>
+      <Header />
       <GlobalStyle />
-      <h1>Hello, world!</h1>
-    </div>
+    </>
   );
 }
