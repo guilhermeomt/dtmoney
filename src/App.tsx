ca5df9caa3c7a39ec350b98b1d5a9b66863db1ea
Guilherme <guilhermet213@gmail.com>
2021-07-19 09:16:06 -0300

    feat: implement global styles
    also add favicon to index page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
-import styled from "styled-components";
-
-const Title = styled.h1`
-  font-size: 2em;
-  font-family: "Open Sans", sans-serif;
-  font-weight: bold;
-  color: red;
-
-  span {
-    color: blue;
-  }
-`;
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
-      <Title>
-        Hello, world <span>!</span>
-      </Title>
+      <GlobalStyle />
+      <h1>Hello, world!</h1>
     </div>
   );
 }
