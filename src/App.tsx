cdad9ce9307f30e21683f17afa3d6da30962b252
Guilherme <guilhermet213@gmail.com>
2021-07-19 08:54:01 -0300

    feat: add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 2em;
+  font-family: "Open Sans", sans-serif;
+  font-weight: bold;
+  color: red;
+
+  span {
+    color: blue;
+  }
+`;
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
+      <Title>
+        Hello, world <span>!</span>
+      </Title>
     </div>
   );
 }
