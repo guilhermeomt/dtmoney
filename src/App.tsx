1e89c8d0598886751189ab94e7ca253d7678dde6
Guilherme Tavares <guilhermet213@gmail.com>
2021-07-20 18:44:39 -0300

    feat: add new transactions modal initial markup and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import { Header } from "./components/Header";
+import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 
 import { GlobalStyle } from "./styles/global";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root');
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
