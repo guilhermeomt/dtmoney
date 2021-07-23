e214f47ad6c04504e5ce456ee9f052ffc929ecd8
Guilherme Tavares <guilhermet213@gmail.com>
2021-07-23 18:57:19 -0300

    feat: create radio box component inside new transactions modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,7 @@
+import { useState } from "react";
 import Modal from "react-modal";
 
-import { Container, TransactionTypeContainer } from "./styles";
+import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 
 import incomeImg from "../../assets/income.svg"
 import outcomeImg from "../../assets/outcome.svg"
@@ -12,6 +13,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const [type, setType] = useState('deposit');
+  
   return (
     <Modal
       isOpen={isOpen}
@@ -41,20 +44,23 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         />
 
         <TransactionTypeContainer>
-          <button
+          <RadioBox
             type="button"
+            onClick={() => { setType("deposit") }}
+            isActive={type === "deposit"}
           >
             <img src={incomeImg} alt="Entrada" />
             <span>Entrada</span>
-          </button>
+          </RadioBox>
 
-          <button
+          <RadioBox
             type="button"
-
+            onClick={() => { setType("withdraw") }}
+            isActive={type === "withdraw"} 
           >
             <img src={outcomeImg} alt="Saída" />
             <span>Saída</span>
-          </button>
+          </RadioBox>
         </TransactionTypeContainer>
 
         <input
