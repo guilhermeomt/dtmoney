dccebafd44b4235dadb0fa9a9e0fbd3e0dd74252
Guilherme Tavares <guilhermet213@gmail.com>
2021-07-23 18:15:31 -0300

    feat: add styles for new transaction modal's form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from "react-modal";
 
-import { Container } from './styles';
+import { Container } from "./styles";
+
+import closeImg from "../../assets/close.svg"
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -15,6 +17,15 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
+
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar" />
+      </button>
+
       <Container>
         <h2>Cadastrar transação</h2>
 
