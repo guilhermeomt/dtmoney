69e4699111386a09b4abca0419bf030357cf9396
Guilherme Tavares <guilhermet213@gmail.com>
2021-07-27 18:14:01 -0300

    feat: retrieve all transactions from api and list them on transactions table

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 
 import { Container } from "./styles";
 
+interface Transaction {
+  id: number,
+  title: string,
+  type: string,
+  amount: number,
+  category: string,
+  createdAt: string,
+}
+
 export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
   useEffect(() => {
-    api("transactions").then((response) => console.log(response.data));
+    api("transactions").then((response) => setTransactions(response.data.transactions));
   }, []);
 
   return (
@@ -20,18 +31,16 @@ export function TransactionsTable() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="deposit">R$12.000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
-          <tr>
-            <td>Aluguel</td>
-            <td className="withdraw">- R$1.100</td>
-            <td>Casa</td>
-            <td>17/02/2021</td>
-          </tr>
+          {
+            transactions.map(transaction => (
+              <tr key={transaction.id}>
+                <td>{transaction.title}</td>
+                <td className={transaction.type}>{transaction.amount}</td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+            ))
+          }
         </tbody>
       </table>
     </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -7,13 +7,35 @@ createServer({
   models: {
     transaction: Model,
   },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelance de website",
+          type: "deposit",
+          category: "Desenvolvimento",
+          amount: 6000,
+          createdAt: new Date("2020-02-12 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1500,
+          createdAt: new Date("2020-02-14 11:00:00"),
+        }
+      ]
+    })
+  },
   routes() {
     this.namespace = "api";
 
     this.get("/transactions", () => {
       return this.schema.all('transaction');
     });
-    
+
     this.post("/transactions", (schema, request) => {
       const data = JSON.parse(request.requestBody);
       return schema.create('transaction', data);
