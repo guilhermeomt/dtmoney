53d0065e22b2da0e6356809f6d3d35823d598a5c
Guilherme Tavares <guilhermet213@gmail.com>
2021-08-03 18:15:35 -0300

    refactor: move transactions context code from useTransactions to another file

---
+++ src/contexts/TransactionsContext.tsx
@@ -0,0 +1,56 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "../services/api";
+
+interface Transaction {
+  id: number,
+  title: string,
+  type: string,
+  amount: number,
+  category: string,
+  createdAt: string,
+}
+
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>
+
+interface TransactionsProviderProps {
+  children: ReactNode;
+}
+
+interface TransactionsContextData {
+  transactions: Transaction[];
+  createTransaction: (transaction: TransactionInput) => void;
+}
+
+export const TransactionsContext = createContext<TransactionsContextData>(
+  {} as TransactionsContextData
+);
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    api("transactions").then((response) => setTransactions(response.data.transactions));
+  }, []);
+
+
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createdAt: new Date(),
+    });
+
+    const { transaction } = response.data;
+    console.log(transaction);
+
+    setTransactions([
+      ...transactions,
+      transaction,
+    ])
+  }
+
+  return (
+    <TransactionsContext.Provider value={{ transactions, createTransaction }}>
+      {children}
+    </TransactionsContext.Provider>
+  );
+}
